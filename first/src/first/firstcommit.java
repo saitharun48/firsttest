430ee9372034e7323574f4c7dcd0cfd4cb05e9db
nsais <nsais@DESKTOP-BH3RKLO>
2024-10-08 16:14:28 +0530

    This is new project

--- first/src/first/firstcommit.java
+++ first/src/first/firstcommit.java
@@ -4,7 +4,8 @@ public class firstcommit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("gello ");
+          System.out.println("hello ");
+          System.out.println("hello world");
 	}
 
 }
